2313f5ab8e71211913284276a480f2f11753ce06
chandra-techstack <svchandrashekarmurthy@gmail.com>
2026-03-20 19:56:56 +0530

    Initial commit for code

--- force-app/main/default/lwc/addressForm/addressForm.js
+++ force-app/main/default/lwc/addressForm/addressForm.js
@@ -6,9 +6,8 @@ import STREET_FIELD from '@salesforce/schema/Venue__c.Street__c';
 import CITY_FIELD from '@salesforce/schema/Venue__c.City__c';
 import STATE_FIELD from '@salesforce/schema/Venue__c.State__c';
 import POSTCODE_FIELD from '@salesforce/schema/Venue__c.PostalCode__c';
-import COUNTRY_FIELD from '@salesforce/schema/Venue__c.Country__c';export default class AddressForm extends LightningElement {
-
-    //DELTA CHANGES TESTING
+import COUNTRY_FIELD from '@salesforce/schema/Venue__c.Country__c';
+export default class AddressForm extends LightningElement {
     address = null;
     @api recordId;
     isLoading = false;
