cd93ae462175080081d977453e71a940de76a268
chandra-techstack <svchandrashekarmurthy@gmail.com>
2026-03-20 17:26:20 +0530

    Added changes in AbstractDMLOperations and AddressForm LWC

--- force-app/main/default/lwc/addressForm/addressForm.js
+++ force-app/main/default/lwc/addressForm/addressForm.js
@@ -6,8 +6,9 @@ import STREET_FIELD from '@salesforce/schema/Venue__c.Street__c';
 import CITY_FIELD from '@salesforce/schema/Venue__c.City__c';
 import STATE_FIELD from '@salesforce/schema/Venue__c.State__c';
 import POSTCODE_FIELD from '@salesforce/schema/Venue__c.PostalCode__c';
-import COUNTRY_FIELD from '@salesforce/schema/Venue__c.Country__c';
-export default class AddressForm extends LightningElement {
+import COUNTRY_FIELD from '@salesforce/schema/Venue__c.Country__c';export default class AddressForm extends LightningElement {
+
+    //DELTA CHANGES TESTING
     address = null;
     @api recordId;
     isLoading = false;
